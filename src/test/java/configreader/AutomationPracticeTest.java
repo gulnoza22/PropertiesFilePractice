2d4987bc6587c63c98a9e718b4ba055ec5b041c7
Gulnoza <tadjichka@gmail.com>
2018-07-11 00:35:33 -0500

    refactored

--- src/test/java/configreader/AutomationPracticeTest.java
+++ src/test/java/configreader/AutomationPracticeTest.java
@@ -1,26 +1,45 @@
 package configreader;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AutomationPracticeTest {
 	WebDriver driver;
-    @Test
-    public void congifReaderTest() throws InterruptedException {
-    	WebDriverManager.chromedriver().setup();
-    	driver=new ChromeDriver();
-    	String url= ConfigLoader.getMyValue("URL");
-    	driver.get(url);
-    	driver.findElement(By.cssSelector(".login")).click();
-    	driver.findElement(By.id("email")).sendKeys(ConfigLoader.getMyValue("User"));
-    	driver.findElement(By.id("passwd")).sendKeys(ConfigLoader.getMyValue("Password"));
-    	driver.findElement(By.id("SubmitLogin")).click();
 
-    	Thread.sleep(3000);
-    	driver.close();
-    } 
+	@BeforeClass
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+	@Test
+	public void congifReaderTest() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		String url = ConfigLoader.getMyValue("URL");
+		driver.get(url);
+		driver.findElement(By.cssSelector(".login")).click();
+		driver.findElement(By.id("email")).sendKeys(ConfigLoader.getMyValue("User"));
+		driver.findElement(By.id("passwd")).sendKeys(ConfigLoader.getMyValue("Password"));
+		driver.findElement(By.id("SubmitLogin")).click();
+
+	}
+
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.close();
+	}
+
 }
